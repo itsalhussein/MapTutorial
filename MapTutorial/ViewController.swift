429265981b9d379e82fbae2b0d963ea03adc723e
Hussein <hussein.elsherief@gmail.com>
2022-09-26 20:02:42 +0200

    - Added MapView and Implemented SetInitialLocation and AddAnnotation Functions.

--- MapTutorial/ViewController.swift
+++ MapTutorial/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let initialLocation = CLLocation(latitude: 30.033333
+, longitude: 31.233334)
+        setInitialLocation(location: initialLocation, distance: 1000)
+        addAnnotation()
     }
 
+    
+    func setInitialLocation(location: CLLocation, distance: CLLocationDistance) {
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: distance, longitudinalMeters: distance)
+        mapView.setRegion(region, animated: true)
+//        mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+        
+    }
+    
+    func addAnnotation(){
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2D(latitude: 30.033333, longitude: 31.233334)
+        mapView.addAnnotation(pin)
+    }
 
 }
 
